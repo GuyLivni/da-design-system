373f1dde2807e22e12dd3e06e7d152459d8beb0a
Guy Livni <guylivni@gmail.com>
2021-02-20 14:10:18 +0100

    init

---
+++ src/Button.tsx
@@ -0,0 +1,21 @@
+import React, { FC, ButtonHTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button.attrs({
+  className: `px-8 py-2 font-semibold text-white transition
+              duration-500 ease-in-out transform rounded-lg
+              shadow-xl bg-gradient-to-r from-red-300 to-blue-300
+              hover:from-pink-400 hover:to-indigo-400`,
+})<{ variant: ButtonVariants }>`
+  color: ${({ variant }) => (variant === 'warning' ? 'red' : '')};
+`;
+
+type ButtonVariants = 'default' | 'warning';
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  variant?: ButtonVariants;
+}
+
+export const Button: FC<ButtonProps> = ({ text, variant = 'default' }) => {
+  return <StyledButton variant={variant}>{text}</StyledButton>;
+};
---
+++ test/Button.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { SCButton as Button } from '../stories/Button.stories';
+
+describe('Button', () => {
+  it('should render the button without crashing', () => {
+    const { getByRole } = render(<Button text='test' />);
+
+    expect(getByRole('button')).toBeInTheDocument();
+  });
+});
